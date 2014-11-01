684bbfe5dbe9cefbeba5570a04559617a379c097
ardila <ardila@mit.edu>
2014-10-31 22:30:44 -0400

    Much cleaner code, added Kaolack and Leumbel rhythms

--- draw_circle.py
+++ draw_circle.py
@@ -1,62 +1,67 @@
 import gizeh
 import numpy as np
 import moviepy.editor as mpy
-subdivisions = [2, 1,  .5]
-W,H = 256*len(subdivisions),256 # width, height, in ixels
-duration = 3 # duration of the clip, in seconds
-cycles = 4  #duration of the clip in cycles
-hits = np.array([0, .75, 1.5, 1.75, 2, 2.75, 3.0, 3.5, 3.75])*2*np.pi
-ta = np.array([ 1.5, 1.75, 3.5, 3.75])*2*np.pi
-gin = np.array([.75, 2.75, 3])*2*np.pi
-pax = np.array([0, 1.25, 2])*2*np.pi
-hits = {}
-hits[0] = ta
-hits[1] = gin
-hits[2] = pax
-#add hits at beginning to get burn in
-for channel in hits.keys():
-    hits[channel] = np.concatenate([hits[channel], hits[channel]+(cycles*2*np.pi)])
-print hits
-fade = 2*np.pi
+from rhythms import Kaolak, Leumbel
+from numpy import pi
+time_multiples = [1, 2, 4, 6, 8]
+W, H = 256, 256 # width, height, in pixels of one window. Total size is W*len(time_multiples), H*len(parts)
+duration = 4 # duration of the clip, in seconds
+
+rhythm = Kaolak()
+parts = ['mbalax', 'talmac', 'nyokos']
+
+stroke_colors = {'gin': [1, 0, 0],
+                 'ta': [0, 1, 0],
+                 'pax': [1, 1, 0],
+                 'rwan': [1, .5, 0],
+                 'tet': [.5, 1, 0]}
+
+stroke_names = stroke_colors.keys()
+stroke_offset_magnitude = .005
+offsets = np.linspace(-stroke_offset_magnitude, stroke_offset_magnitude, len(stroke_names)) - pi/2
+stroke_offsets = {stroke: offset for stroke, offset in zip(stroke_names, offsets)}
+
+fade = np.pi/2.
+
 def ray(R, theta, center, **kw):
     x,y = center
     dx,dy = gizeh.polar2cart(R, theta)
     return gizeh.polyline(points=[(x,y), (x+dx,y+dy)], **kw)
 
-blank = gizeh.Surface(W,H).get_npimage()
-surface = gizeh.Surface(W,H)
 
 
-def spinner(t, center, subdivision, R, channel):
-#    image = blank 
-    theta = (t+duration)/duration*2*np.pi*cycles*subdivision 
-    for hit in hits[channel]:
-        hit = hit*subdivision
-        #surface = gizeh.Surface(W,H)
+
+def spinner(t, time_multiple, center, surface, R, hits, phase_offset, stroke_color):
+    theta = t/duration*2*np.pi*time_multiple
+    for hit in hits:
+        hit = hit*time_multiple
         if theta>hit:
-            i = np.exp((hit-theta)/fade/subdivision)
-            stroke = np.array({0:[0,1,0], 1:[1,0,0], 2:[1,1,0]}[channel])
-            stroke = stroke*i
-            ray(R, hit-(np.pi/2)+(channel-1)*.005*2*np.pi, center, stroke_width=1, stroke=tuple(stroke)).draw(surface)
- #           image += surface.get_npimage()
-  #  return image
-#    ray(R, theta, center, stroke_width=1, stroke=(1,1,0)).draw(surface) 
+            intensity = np.exp((hit-theta)/fade/time_multiple)
+            stroke = np.array(stroke_color)*intensity
+            if intensity > .01:
+                ray(R, hit, center, stroke_width=1, stroke=tuple(stroke)).draw(surface)
+
+
+columns = len(time_multiples)
+rows = len(parts)
+
+
 
 def make_frame(t):
-    image = blank
-    xcenter = H/2
-    ycenter = H/2
-    for i, subdivision in enumerate(reversed(subdivisions)): 
-       for channel in hits.keys(): 
-            spinner(t, (xcenter, ycenter), subdivision, H/2, channel )         
-       xcenter+=H
-       # ycenter += 128
-       # xcenter = 64
-        
-      
-   # for i in range(image.shape[2]):
-#	image[:,:,i] = np.max(image[:,:,i])
-#    return image 
+    surface = gizeh.Surface(W*columns, H*rows)
+    xcenters = np.linspace(W/2, (columns-1)*W + W/2, columns)
+    ycenters = np.linspace(H/2, (rows-1)*H + H/2, rows)
+    for xcenter, time_multiple in zip(xcenters, time_multiples):
+        for ycenter, part in zip(ycenters, parts):
+
+            part = getattr(rhythm, part)
+            for stroke in part.keys():
+                stroke_color = stroke_colors[stroke]
+                hits = part[stroke]
+                phase_offset = stroke_offsets[stroke]
+                spinner(t, time_multiple, (xcenter, ycenter), surface, W/2, hits, phase_offset, stroke_color)
     return surface.get_npimage()
+
+
 clip = mpy.VideoClip(make_frame, duration=duration)
-clip.write_gif("circle.gif",fps=60, opt="OptimizePlus", fuzz=10)
+clip.write_gif("circle1.gif",fps=60, opt="OptimizePlus", fuzz=10)
---
+++ rhythms.py
@@ -0,0 +1,53 @@
+__author__ = 'ardila'
+
+import numpy as np
+from numpy import pi
+
+
+class Rhythm(object):
+   def __init__(self, repeats=2):
+        for part in [self.mbalax, self.talmac, self.nyokos]:
+            for stroke in part.keys():
+                hits = np.array(part[stroke])
+                if self.triple_meter == True:
+                    hits = hits/3.
+                hits = hits / 4. # 4 beats per cycle
+                hits = hits * 2 * pi #1 measure per 2 pi radians
+                final_hits = hits
+                offsets = np.linspace(-(repeats-1)*2*pi, 0, repeats)
+                # add repeats before start of video loop for burn-in
+                part[stroke] = np.concatenate([repeat+offset for repeat, offset in zip([hits]*repeats, offsets)])
+
+
+class Kaolak(Rhythm):
+    triple_meter = False
+
+    mbalax = {'ta': [1.5, 1.75, 3.5, 3.75],
+              'gin': [.75, 2.75, 3],
+              'pax': [0, 1.25, 2]}
+
+    talmac = {'ta': [3.75],
+              'gin': [0, 2],
+              'tet': [.5, 2.5],
+              'rwan': [1.5],
+              'pax': [3]}
+
+    nyokos = {'ta': [0, .75, 1.5, 2.75],
+              'gin': [1, 3],
+              'pax': [1.75]}
+
+
+class Leumbel(Rhythm):
+    triple_meter = True
+    mbalax = {'ta': [0, 1, 3, 4, 6, 7, 9, 10],
+              'gin': [2, 8],
+              'pax': [5, 11]}
+
+    talmac = {'ta': [5, 11],
+              'gin': [0, 6],
+              'tet': [2, 4, 7, 9]}
+
+    nyokos = {'ta': [0, 2, 4, 8],
+              'gin': [3, 9],
+              'pax': [5]}
+
